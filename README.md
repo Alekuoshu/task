35c03f77398b78654b12a9f3b0ee1d8ca320f226
Alekuoshu <15611830+Alekuoshu@users.noreply.github.com>
2024-06-18 19:11:33 -0500

    demo link

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React App: CRUD Task List
 
 This is a React App for beginners, this help to understand basic knowledge about react: components, JSX, states.
+
+Project Link Demo: https://jazzy-cheesecake-c6fa96.netlify.app/
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,21 @@ import React, { Component } from "react";
 
 // Component for footer section
 class Footer extends Component {
-    render() {
-      return (
-        <p>Development by <a href="https://github.com/Alekuoshu" target="_blank" rel="noopener noreferrer">Alekuoshu</a> | April 2023</p>
-      );
-    }
+  render() {
+    return (
+      <p>
+        Development by{" "}
+        <a
+          href="https://github.com/Alekuoshu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Alekuoshu
+        </a>{" "}
+        | April 2023
+      </p>
+    );
   }
-  
-  export default Footer;
+}
+
+export default Footer;
